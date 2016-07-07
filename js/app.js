cc3542c5fefc0b73cc1ecdb7ce545c3f763ca272
jig08 <grover.jigyasa1@gmail.com>
2016-07-07 13:20:39 -0400

    Basic Structure of web app created.

---
+++ js/app.js
@@ -0,0 +1,56 @@
+// app.js
+// create our angular app and inject ngAnimate and ui-router 
+// =============================================================================
+angular.module('formApp', ['ngAnimate', 'ui.router'])
+
+// configuring our routes 
+// =============================================================================
+.config(function($stateProvider, $urlRouterProvider) {
+    
+    $stateProvider
+    
+        // route to show our basic form (/form)
+        .state('form', {
+            url: '/form',
+            templateUrl: 'forms/form.html',
+            controller: 'formController'
+        })
+        
+        // nested states 
+        // each of these sections will have their own view
+        // url will be nested (/form/profile)
+        .state('form.profile', {
+            url: '/profile',
+            templateUrl: 'forms/form-profile.html'
+        })
+        
+        // url will be /form/interests
+        .state('form.ques1', {
+            url: '/ques1',
+            templateUrl: 'forms/ques1.html'
+        })
+        
+        // url will be /form/payment
+        .state('form.lastQues', {
+            url: '/lastQues',
+            templateUrl: 'forms/lastQues.html'
+        });
+        
+    // catch all route
+    // send users to the form page 
+    $urlRouterProvider.otherwise('/form/profile');
+})
+
+// our controller for the form
+// =============================================================================
+.controller('formController', function($scope) {
+    
+    // we will store all of our form data in this object
+    $scope.formData = {};
+    
+    // function to process the form
+    $scope.processForm = function() {
+        alert('awesome!');
+    };
+    
+});
