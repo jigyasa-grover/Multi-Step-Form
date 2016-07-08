a726d7f260d3a6db23dddfdd1b7a90a06ada136c
jig08 <grover.jigyasa1@gmail.com>
2016-07-08 11:16:49 -0400

    Cleaned code, and added stuff (I dont remember)

--- js/app.js
+++ js/app.js
@@ -1,9 +1,9 @@
 
-// create our angular app and inject ngAnimate and ui-router 
+// Creating angular app and inject ngAnimate and ui-router 
 // =============================================================================
 angular.module('formApp', ['ngAnimate', 'ui.router'])
 
-// configuring our routes 
+// Configuring our routes 
 // =============================================================================
 .config(function($stateProvider, $urlRouterProvider) {
     
@@ -36,21 +36,21 @@ angular.module('formApp', ['ngAnimate', 'ui.router'])
             templateUrl: 'forms/lastQues.html'
         });
         
-    // catch all route
-    // send users to the form page 
+    // Catch all route
+    // Send users to the form page 
     $urlRouterProvider.otherwise('/form/profile');
 })
 
-// our controller for the form
+// Form Controller
 // =============================================================================
 .controller('formController', function($scope) {
     
-    // we will store all of our form data in this object
+    // Storing form data in this object
     $scope.formData = {};
     
-    // function to process the form
+    // Function to process the form
     $scope.processForm = function() {
-        alert('awesome!');
+        alert('Awesome!');
     };
     
 });
