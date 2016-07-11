e24b607aebb436b023aae93c5d74b643816d30a9
jig08 <grover.jigyasa1@gmail.com>
2016-07-11 14:53:49 -0400

    Scrubbing code..

--- js/app.js
+++ js/app.js
@@ -1,4 +1,3 @@
-
 // Creating angular app and inject ngAnimate and ui-router 
 // =============================================================================
 angular.module('formApp', ['ngAnimate', 'ui.router'])
@@ -6,66 +5,60 @@ angular.module('formApp', ['ngAnimate', 'ui.router'])
 // Configuring our routes 
 // =============================================================================
 .config(function($stateProvider, $urlRouterProvider) {
-    
+
     $stateProvider
-    
-        // route to show our basic form (/form)
+
+    // route to show our basic form (/form)
         .state('form', {
-            url: '/form',
-            templateUrl: 'forms/form.html',
-            controller: 'formController'
-        })
-        
-        // nested states 
-        // each of these sections will have their own view
-        // url will be nested (/form/profile)
-        .state('form.profile', {
-            url: '/profile',
-            templateUrl: 'forms/form-profile.html'
-        })
+        url: '/form',
+        templateUrl: 'forms/form.html',
+        controller: 'formController'
+    })
 
-        .state('form.ques-numberOfAgents', {
-            url: '/ques-numberOfAgents',
-            templateUrl: 'forms/ques-numberOfAgents.html'
-        })
+    // nested states 
+    // each of these sections will have their own view
+    // url will be nested (/form/profile)
+    .state('form.profile', {
+        url: '/profile',
+        templateUrl: 'forms/form-profile.html'
+    })
 
-        .state('form.ques-typesOfAgents', {
-            url: '/ques-typesOfAgents',
-            templateUrl: 'forms/ques-typesOfAgents.html'
-        })
+    .state('form.ques-numberOfAgents', {
+        url: '/ques-numberOfAgents',
+        templateUrl: 'forms/ques-numberOfAgents.html'
+    })
 
-        .state('form.ques-maxTime', {
-            url: '/ques-maxTime',
-            templateUrl: 'forms/ques-maxTime.html'
-        })
+    .state('form.ques-typesOfAgents', {
+        url: '/ques-typesOfAgents',
+        templateUrl: 'forms/ques-typesOfAgents.html'
+    })
 
-        .state('form.ques-maxLoops', {
-            url: '/ques-maxLoops',
-            templateUrl: 'forms/ques-maxLoops.html'
-        })
+    .state('form.ques-maxTime', {
+        url: '/ques-maxTime',
+        templateUrl: 'forms/ques-maxTime.html'
+    })
 
-        .state('form.ques-hashtagProb', {
-            url: '/ques-hashtagProb',
-            templateUrl: 'forms/ques-hashtagProb.html'
-        })
-        
-        // url will be /form/ques1
-        .state('form.ques1', {
-            url: '/ques1',
-            templateUrl: 'forms/ques1.html'
-        })
+    .state('form.ques-maxLoops', {
+        url: '/ques-maxLoops',
+        templateUrl: 'forms/ques-maxLoops.html'
+    })
 
-        .state('form.ques-rates', {
-            url: '/ques-rates',
-            templateUrl: 'forms/ques-rates.html'
-        })
-        
-        // url will be /form/lastQues
-        .state('form.lastQues', {
-            url: '/lastQues',
-            templateUrl: 'forms/lastQues.html'
-        });
-        
+    .state('form.ques-hashtagProb', {
+        url: '/ques-hashtagProb',
+        templateUrl: 'forms/ques-hashtagProb.html'
+    })
+
+    .state('form.ques-rates', {
+        url: '/ques-rates',
+        templateUrl: 'forms/ques-rates.html'
+    })
+
+    // url will be /form/lastQues
+    .state('form.lastQues', {
+        url: '/lastQues',
+        templateUrl: 'forms/lastQues.html'
+    });
+
     // Catch all route
     // Send users to the form page 
     $urlRouterProvider.otherwise('/form/profile');
@@ -74,13 +67,13 @@ angular.module('formApp', ['ngAnimate', 'ui.router'])
 // Form Controller
 // =============================================================================
 .controller('formController', function($scope) {
-    
+
     // Storing form data in this object
     $scope.formData = {};
-    
+
     // Function to process the form
     $scope.processForm = function() {
         alert('Awesome!');
     };
-    
+
 });
